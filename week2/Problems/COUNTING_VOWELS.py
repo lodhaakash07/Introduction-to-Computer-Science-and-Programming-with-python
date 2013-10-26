501d1a9e45653dbdafae5094b9811c19a4d2d339
xala3pa <salazar3pa@gmail.com>
2013-10-26 10:24:50 +0200

    Counting Vowels Problem

---
+++ week2/Problems/COUNTING_VOWELS.py
@@ -0,0 +1,11 @@
+s = 'azcbobobegghakla8ufghtre'
+
+def count_vowels(s): 
+    vowels = 'aeiou'
+    counter = 0
+    for vowel in s:
+        if vowel in vowels:
+            counter += 1
+    return counter
+ 
+print count_vowels(s)      

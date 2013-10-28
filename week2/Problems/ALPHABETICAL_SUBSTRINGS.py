7b7c58fada66e27a00b339618a1933075f70a6dd
xala3pa <salazar3pa@gmail.com>
2013-10-28 12:29:27 +0100

    Change variable name

--- week2/Problems/ALPHABETICAL_SUBSTRINGS.py
+++ week2/Problems/ALPHABETICAL_SUBSTRINGS.py
@@ -1,15 +1,15 @@
-s = 'usuustzehxwhrmmp'
+s = 'usuustzabcfgehxwhrmmp'
 
 def get_alphabetical_substring(s):
-    point = ''
+    offset = ''
     ans = ''
     temp = ''
     for letter in s: 
-        if letter >= point:            
+        if letter >= offset:            
             temp += letter
         else:                        
             temp = letter   
-        point = letter
+        offset = letter
         if len(temp) > len(ans):               
                 ans = temp                                     
     return ans

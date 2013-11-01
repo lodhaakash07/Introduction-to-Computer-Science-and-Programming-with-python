85557f828e9404da9837e5fcc9ca83478ce51845
xala3pa <salazar3pa@gmail.com>
2013-11-01 20:18:05 +0100

    Length

---
+++ week3/Problems/L5PROBLEM6.py
@@ -0,0 +1,11 @@
+def lenIter(aStr):
+    '''
+    aStr: a string
+    
+    returns: int, the length of aStr
+    '''
+    total = 0
+    for c in aStr:
+        total += 1
+    return total
+        

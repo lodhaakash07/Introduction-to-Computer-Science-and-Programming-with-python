2383986cfd7cc7b9a4c27037f6ad0ca54b78b814
xala3pa <salazar3pa@gmail.com>
2013-11-26 23:31:05 +0100

    Lecture 11 Problem

--- week6/Lectures/Lecture_11.py
+++ week6/Lectures/Lecture_11.py
@@ -1,36 +1,11 @@
-class A(object):
-    def __init__(self):
-        self.a = 1
-    def x(self):
-        print "A.x"
-    def y(self):
-        print "A.y"
-    def z(self):
-        print "A.z"
-
-class B(A):
-    def __init__(self):
-        A.__init__(self)
-        self.a = 2
-        self.b = 3
-    def y(self):
-        print "B.y"
-    def z(self):
-        print "B.z"
-
-class C(object):
-    def __init__(self):
-        self.a = 4
-        self.c = 5
-    def y(self):
-        print "C.y"
-    def z(self):
-        print "C.z"
-
-class D(C, B):
-    def __init__(self):
-        C.__init__(self)
-        B.__init__(self)
-        self.d = 6
-    def z(self):
-        print "D.z"
+def genPrimes():
+    index = 1
+    prime_list = []
+    while True:
+        index += 1
+        for i in prime_list:
+            if index % i == 0:
+                break
+        else:
+            prime_list.append(index)
+            yield index
